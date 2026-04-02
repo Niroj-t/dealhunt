3d19a070e123b20032e82982cadf514681a7d5ec
Niroj Thapa <you@niroj.com>
2026-04-02 11:55:06 +0545

    basic functionalities

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import { Toaster } from "@/components/ui/sonner";
+import "./globals.css";
+
+export const metadata = {
+  title: "Price Tracker",
+  description:
+    "Track product prices across e-commerce sites and get alerts on price drops",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+
+        <Toaster richColors />
+      </body>
+    </html>
+  );
+}
